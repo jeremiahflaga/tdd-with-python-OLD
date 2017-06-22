9373e2eb9042705c390fc03952da8b4e57de7404
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2017-06-22 08:55:40 +0800

    modifications to functional tests because there are errors :)

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -3,19 +3,20 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def teardown(self):
+    def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        assert 'To-Do' in browser.title
+        self.assertIn('To-Do', self.browser.title)
+        #self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
@@ -38,7 +39,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
-        browser.quit()
+        self.browser.quit()
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
